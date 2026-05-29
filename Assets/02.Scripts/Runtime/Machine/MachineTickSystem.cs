f91f0d4b36824dde3c39d9e8b718f993fc434317
박서진 <kevin8193@naver.com>
2026-05-29 17:50:46 +0900

    fixing consume/produce

--- Assets/02.Scripts/Runtime/Machine/MachineTickSystem.cs
+++ Assets/02.Scripts/Runtime/Machine/MachineTickSystem.cs
@@ -43,6 +43,11 @@ namespace MokoIndustry.Machine
                 {
                     ms.ProgressTicks = 0;
                 }
+                else
+                {
+                    ms.ProgressTicks = recipe.ProcessingTicks;
+                    return;
+                }
             }
 
             // try to start (same tick)
@@ -59,7 +64,20 @@ namespace MokoIndustry.Machine
             }
         }
 
-        private bool TryProduce(ref FixedList64Bytes<byte> buf, in FixedList32Bytes<RecipeItem> req)
+        private bool TryProduce(ref FixedList64Bytes<byte> buf, in FixedList32Bytes<RecipeItem> outs)
+        {
+            // is there some space?
+            int total = 0;
+            for (int i = 0; i < outs.Length; i++) total += outs[i].Count;
+            if (buf.Length + total > buf.Capacity) return false;
+
+            for (int i = 0; i < outs.Length; i++)
+                for (int c = 0; c < outs[i].Count; c++)
+                    buf.Add(outs[i].ItemId);
+            return true;
+        }
+
+        private bool TryConsume(ref FixedList64Bytes<byte> buf, in FixedList32Bytes<RecipeItem> req)
         {
             // requirement check
             for (int i = 0; i < req.Length; i++)
@@ -79,19 +97,6 @@ namespace MokoIndustry.Machine
                     if (buf[j] == req[i].ItemId) { buf.RemoveAt(j); remain--; }
                 }
             }
-            return true;
-        }
-
-        private bool TryConsume(ref FixedList64Bytes<byte> buf, in FixedList32Bytes<RecipeItem> outs)
-        {
-            // is there some space?
-            int total = 0;
-            for (int i = 0; i < outs.Length; i++) total += outs[i].Count;
-            if (buf.Length + total > buf.Capacity) return false;
-
-            for (int i = 0; i < outs.Length; i++)
-                for (int c = 0; c < outs[i].Count; c++)
-                    buf.Add(outs[i].ItemId);
             return true;
         }
     }
